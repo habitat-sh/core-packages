827a3cc8a2ba2184da039fe2e9213ea216ef8496
Johny Jose <johny.jose@progress.com>
2022-11-07 06:52:54 +0000

    fixes patchelf crash
    * build-tools-patchelf: build patchelf with build-tools-gcc instead of
       cross compiling
    * bzip2: adds bzip2 plan
    
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- bootstrap/cross-tools/build-tools-patchelf/plan.sh
+++ bootstrap/cross-tools/build-tools-patchelf/plan.sh
@@ -3,7 +3,7 @@ native_target="${TARGET_ARCH:-${pkg_target%%-*}}-hab-linux-gnu"
 
 pkg_name="build-tools-patchelf"
 pkg_origin="core"
-pkg_version="0.15.0"
+pkg_version="0.16.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 A small utility to modify the dynamic linker and RPATH of ELF executables.\
@@ -11,16 +11,15 @@ A small utility to modify the dynamic linker and RPATH of ELF executables.\
 pkg_upstream_url="https://nixos.org/patchelf.html"
 pkg_license=('GPL-3.0-or-later')
 pkg_source="https://github.com/NixOS/patchelf/archive/refs/tags/${pkg_version}.tar.gz"
-pkg_shasum="5d1eda9bad4439bb9c0a87fcba198d7c0d372f361e77de41d68d6308062e5000"
+pkg_shasum="50e07dd71e2bb88fff043aa6b126b413b0951da848498fe0f2ad38af6650d405"
 pkg_dirname="${program}-${pkg_version}"
 
 pkg_deps=(
     core/build-tools-glibc
-    core/build-tools-libstdcpp
 )
 
 pkg_build_deps=(
-    core/native-cross-gcc
+    core/build-tools-gcc
 )
 
 pkg_bin_dirs=(bin)
@@ -29,9 +28,7 @@ pkg_bin_dirs=(bin)
 do_build() {
     ./bootstrap.sh
     ./configure \
-        --prefix="$pkg_prefix" \
-        --build="$(build-aux/config.guess)" \
-        --host="$native_target"
+        --prefix="$pkg_prefix"
     make
 }
 
---
+++ standard/bootstrapped/bzip2/plan.sh
@@ -0,0 +1,96 @@
+program="bzip2"
+pkg_name="bzip2"
+pkg_origin="core"
+pkg_version="1.0.8"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="bzip2 is a free and open-source file compression program that uses the Burrows–Wheeler algorithm. It only compresses single files and is not a file archiver."
+pkg_upstream_url="https://www.sourceware.org/bzip2"
+pkg_license=('GPL-2.0-or-later' 'LGPL-2.1-or-later')
+pkg_source="https://fossies.org/linux/misc/${program}-${pkg_version}.tar.gz"
+pkg_shasum="ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269"
+pkg_dirname="${program}-${pkg_version}"
+
+pkg_deps=(
+    core/glibc
+)
+
+pkg_build_deps=(
+    core/linux-headers
+    core/build-tools-gcc
+    core/build-tools-make
+    core/build-tools-coreutils
+    core/build-tools-patchelf
+)
+
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+
+do_prepare() {
+    case $pkg_target in
+    aarch64-linux)
+        HAB_GLIBC_DYNAMIC_LINKER="$(pkg_path_for glibc)/lib/ld-linux-aarch64.so.1"
+        ;;
+    x86_64-linux)
+        HAB_GLIBC_DYNAMIC_LINKER="$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2"
+        ;;
+    esac
+    export HAB_GLIBC_DYNAMIC_LINKER
+    build_line "Setting HAB_GLIBC_DYNAMIC_LINKER=${HAB_GLIBC_DYNAMIC_LINKER}"
+
+    HAB_GLIBC_PKG_PATH="$(pkg_path_for glibc)"
+    export HAB_GLIBC_PKG_PATH
+    build_line "Setting HAB_GLIBC_PKG_PATH=${HAB_GLIBC_PKG_PATH}"
+
+    HAB_LINUX_HEADERS_PKG_PATH="$(pkg_path_for linux-headers)"
+    export HAB_LINUX_HEADERS_PKG_PATH
+    build_line "Setting HAB_LINUX_HEADERS_PKG_PATH=${HAB_LINUX_HEADERS_PKG_PATH}"
+
+    unset LDFLAGS
+    unset LD_RUN_PATH
+    unset CFLAGS
+    unset CXXFLAGS
+    unset CPPFLAGS
+    build_line "Unset CFLAGS, CXXFLAGS, CPPFLAGS, LDFLAGS and LD_RUN_PATH"
+
+    # Makes the symbolic links in installation relative vs. absolute
+    # shellcheck disable=SC2016
+    sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
+
+    # Ensure that the man pages are installed under share/man
+    sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
+
+    export HAB_DEBUG=1
+
+}
+
+do_build() {
+    # We add the '-Wl,-rpath' flag to ensure that the bzip-shared binary contains the final location
+    # of the libbz2 shared library
+    make -f Makefile-libbz2_so CFLAGS="-Wl,-rpath=${pkg_prefix}/lib" PREFIX="$pkg_prefix" CC="gcc"
+    make CC="gcc"
+}
+
+do_check() {
+    make test
+}
+
+do_install() {
+    make install PREFIX="$pkg_prefix"
+
+    mkdir -p "$pkg_prefix/lib"
+
+    # Copy shared library
+    cp -av libbz2.so.1.0.8 "$pkg_prefix/lib"
+    ln -sv libbz2.so.1.0.8 "$pkg_prefix/lib/libbz2.so"
+    ln -sv libbz2.so.1.0.8 "$pkg_prefix/lib/libbz2.so.1.0"
+
+    # Copy shared binary to final location
+    cp -av bzip2-shared "$pkg_prefix/bin/bzip2"
+    ln -sfv bzip2 "$pkg_prefix/bin/bzcat"
+    ln -sfv bzip2 "$pkg_prefix/bin/bunzip2"
+
+    # Removes unnecesary rpath entry to build-tools-gcc/lib64
+    patchelf --shrink-rpath "${pkg_prefix}/bin/bzip2"
+    patchelf --shrink-rpath "${pkg_prefix}/bin/bzip2recover"
+
+}
