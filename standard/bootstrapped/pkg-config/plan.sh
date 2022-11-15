3945529c0aa968dca58be0ec35a93c44d4df238d
Sangameshwar Mandakanalli <Sangameshwar.mandakanalli@progress.com>
2022-11-15 10:21:31 +0000

    pkg-config package added
    Signed-off-by: Sangameshwar Mandakanalli <Sangameshwar.mandakanalli@progress.com>

---
+++ standard/bootstrapped/pkg-config/plan.sh
@@ -0,0 +1,43 @@
+pkg_name=pkg-config
+pkg_origin=core
+pkg_version=0.29.2
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+pkg-config is a helper tool used when compiling applications and libraries. \
+It helps you insert the correct compiler options on the command line so an \
+application can use a call to pkg-config for instance, rather than \
+hard-coding values on where to find glib (or other libraries).\
+"
+pkg_upstream_url="http://pkgconfig.freedesktop.org/wiki/"
+pkg_license=('GPL-2.0-or-later')
+pkg_source="http://pkgconfig.freedesktop.org/releases/${pkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/build-tools-coreutils
+  core/build-tools-diffutils
+  core/build-tools-patch
+  core/build-tools-patchelf
+  core/build-tools-make
+  core/build-tools-gcc
+)
+pkg_bin_dirs=(bin)
+
+do_build() {
+  ./configure \
+    --prefix="$pkg_prefix" \
+    --with-internal-glib \
+    --disable-host-tool
+  make
+}
+
+do_check() {
+  make check
+}
+do_install() {
+	make install
+        patchelf --shrink-rpath "$pkg_prefix/bin/pkg-config"
+}
+
