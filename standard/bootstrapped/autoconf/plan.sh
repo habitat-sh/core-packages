efe81ffe17949a66b2d2602c33c0c189475ff61b
raviduddela <rduddela@progress.com>
2022-11-16 06:43:05 +0000

    add sup dependencies
    Signed-off-by: raviduddela <rduddela@progress.com>

---
+++ standard/bootstrapped/autoconf/plan.sh
@@ -0,0 +1,24 @@
+program=autoconf
+pkg_name=autoconf
+pkg_origin=core
+pkg_version=2.71
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Autoconf is an extensible package of M4 macros that produce shell scripts to \
+automatically configure software source code packages.\
+"
+pkg_upstream_url="https://www.gnu.org/software/autoconf/autoconf.html"
+pkg_license=('GPL-2.0-or-later')
+pkg_source="http://ftp.gnu.org/gnu/${program}/${program}-${pkg_version}.tar.xz"
+pkg_shasum="f14c83cfebcc9427f2c3cea7258bd90df972d92eb26752da4ddad81c87a0faa4"
+pkg_deps=(
+  core/m4
+  core/build-tools-perl
+)
+pkg_build_deps=(
+  core/build-tools-diffutils
+  core/inetutils
+  core/gcc-bootstrap
+  core/build-tools-make
+)
+pkg_bin_dirs=(bin)
