57505b372c6e7233257b76f53ec60378d06482d8
Nimit <nimit.jyotiana@hotmail.com>
2022-12-02 11:30:50 +0000

    add autoconf planfile
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

---
+++ final/autoconf/plan.sh
@@ -0,0 +1,25 @@
+pkg_name="autoconf"
+pkg_origin="core"
+pkg_version="2.71"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+Autoconf is an extensible package of M4 macros that produce shell scripts to \
+automatically configure software source code packages.\
+"
+pkg_upstream_url="https://www.gnu.org/software/autoconf/autoconf.html"
+pkg_license=('GPL-2.0-or-later')
+pkg_source="http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="f14c83cfebcc9427f2c3cea7258bd90df972d92eb26752da4ddad81c87a0faa4"
+
+pkg_build_deps=(
+	core/gcc
+	core/m4
+	core/perl
+)
+
+pkg_bin_dirs=(bin)
+
+do_build() {
+	./configure --prefix=$pkg_prefix
+	make
+}
