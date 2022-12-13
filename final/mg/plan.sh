4ae5a28397bf873aaab4e5a950cba041f97a1b82
Sangameshwar Mandakanalli <Sangameshwar.mandakanalli@progress.com>
2022-12-13 11:54:05 +0000

    mg package added, required for hab sup
    Signed-off-by: Sangameshwar Mandakanalli <Sangameshwar.mandakanalli@progress.com>

---
+++ final/mg/plan.sh
@@ -0,0 +1,54 @@
+pkg_name=mg
+pkg_origin=core
+pkg_version=20221112
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+mg is Micro GNU/emacs, this is a portable version of the mg maintained by the \
+OpenBSD team.\
+"
+pkg_upstream_url="https://homepage.boetes.org/software/mg/"
+pkg_license=('publicdomain')
+pkg_source="https://github.com/hboetes/$pkg_name/archive/$pkg_version.tar.gz"
+pkg_shasum="eeb4181fdb6f2bd3f9b5a3c8ccc5b71fbcf9c7f4a805a2f1858334359d44b545"
+pkg_deps=(
+  core/glibc
+  core/ncurses
+  core/libbsd
+  core/libmd
+)
+pkg_build_deps=(
+  core/coreutils
+  core/diffutils
+  core/patch
+  core/make
+  core/gcc
+  core/sed
+  core/pkg-config
+  core/clens
+)
+pkg_bin_dirs=(bin)
+
+do_prepare() {
+  # shellcheck disable=SC2002
+  cat "$PLAN_CONTEXT/cleanup.patch" \
+    | sed \
+      -e "s,@prefix@,$pkg_prefix,g" \
+      -e "s,@clens_prefix@,$(pkg_path_for clens),g" \
+      -e "s,@libbsd_prefix@,$(pkg_path_for libbsd),g" \
+    | patch -p1
+}
+
+do_build() {
+  make \
+    prefix="$pkg_prefix" \
+    PKG_CONFIG=pkg-config \
+    INSTALL=install \
+    STRIP=strip
+}
+
+do_install() {
+  do_default_install
+
+  # Install license file from README
+  install -Dm644 README "$pkg_prefix/share/licenses/README"
+}
