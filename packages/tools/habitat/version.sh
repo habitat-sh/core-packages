7b6b9110df03774949d542cfd6cda8823f825957
Johny Jose <johny.jose@progress.com>
2023-01-18 07:08:11 +0000

    fix habitat studio builds
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- packages/tools/habitat/version.sh
+++
@@ -1,3 +0,0 @@
-commit_hash="707b086fec72a59aa439fedbc9bf032597d59afc"
-git_url="https://github.com/habitat-sh/habitat.git"
-pkg_shasum="5a8766662c0ea0d3932451c520dd31eadf7deda0b6652020ff61ba3f8c3f3773"
