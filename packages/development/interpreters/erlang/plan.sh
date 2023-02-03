f634619f352bc59a5daed75f7633108177b98df8
Johny Jose <johny.jose@progress.com>
2023-02-03 08:56:09 +0000

    remove unnecessray rpaths from erlang25
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- packages/development/interpreters/erlang/plan.sh
+++ packages/development/interpreters/erlang/plan.sh
@@ -10,61 +10,53 @@ pkg_filename="otp_src_${pkg_version}.tar.gz"
 pkg_shasum=d67312017464529603f60a36f4bfb891b6cf7637a375abb27c92ee79ecd14cd4
 pkg_dirname="otp_src_${pkg_version}"
 pkg_build_deps=(
-  core/coreutils
-  core/gcc
-  core/make
-  core/openssl
-  core/perl
-  core/m4
+	core/bison
+	core/gcc
+	core/make
+	core/openssl
+	core/patch
+	core/perl
+	core/m4
 )
 pkg_deps=(
-  core/gcc-libs
-  core/glibc
-  core/zlib
-  core/ncurses
-  core/openssl
-  core/sed
+	core/coreutils
+	core/gcc-libs
+	core/glibc
+	core/zlib
+	core/ncurses
+	core/openssl
 )
 pkg_bin_dirs=(bin)
-pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
 
 do_prepare() {
-  # The `/bin/pwd` path is hardcoded, so we'll add a symlink if needed.
-  if [[ ! -r /bin/pwd ]]; then
-    ln -sv "$(pkg_path_for coreutils)/bin/pwd" /bin/pwd
-    _clean_pwd=true
-  fi
+	# We eliminate extra default platform-specific RPATHs to OpenSSL crypto libraries
+	# from the internal Erlang libraries to prevent accidental usage of the host
+	# system's crypto library.
+	patch -p1 <"$PLAN_CONTEXT/remove_ssl_rpaths.patch"
 
-  if [[ ! -r /bin/rm ]]; then
-    ln -sv "$(pkg_path_for coreutils)/bin/rm" /bin/rm
-    _clean_rm=true
-  fi
+	# Replace all host system env interpreters with our packaged env
+	grep -lr '/usr/bin/env' . | while read -r f; do
+		sed -e "s,/usr/bin/env,$(pkg_interpreter_for coreutils bin/env),g" -i "$f"
+	done
 }
 
 do_build() {
-  sed -i 's/std_ssl_locations=.*/std_ssl_locations=""/' erts/configure
-  CFLAGS="${CFLAGS} -O2" ./configure \
-    --prefix="${pkg_prefix}" \
-    --enable-threads \
-    --enable-smp-support \
-    --enable-kernel-poll \
-    --enable-dynamic-ssl-lib \
-    --enable-shared-zlib \
-    --enable-hipe \
-    --with-ssl="$(pkg_path_for openssl)" \
-    --with-ssl-include="$(pkg_path_for openssl)/include" \
-    --without-javac
-  make
+	sed -i 's/std_ssl_locations=.*/std_ssl_locations=""/' erts/configure
+	CFLAGS="${CFLAGS} -O2" ./configure \
+		--prefix="${pkg_prefix}" \
+		--enable-threads \
+		--enable-smp-support \
+		--enable-kernel-poll \
+		--enable-dynamic-ssl-lib \
+		--enable-shared-zlib \
+		--enable-hipe \
+		--with-ssl="$(pkg_path_for openssl)" \
+		--with-ssl-include="$(pkg_path_for openssl)/include" \
+		--without-javac
+	make
 }
 
-do_end() {
-  # Clean up the `pwd` link, if we set it up.
-  if [[ -n "$_clean_pwd" ]]; then
-    rm -fv /bin/pwd
-  fi
-
-  if [[ -n "$_clean_rm" ]]; then
-    rm -fv /bin/rm
-  fi
+do_check() {
+	make test
 }
