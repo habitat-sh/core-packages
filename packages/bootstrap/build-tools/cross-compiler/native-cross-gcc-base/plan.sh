60f36b045ae401ef9ae80beded02ccdba3f88a70
raviduddela <rduddela@progress.com>
2023-05-03 11:30:22 +0000

    downgrade native binutils,gcc,linuxheaders
    Signed-off-by: raviduddela <rduddela@progress.com>

--- packages/bootstrap/build-tools/cross-compiler/native-cross-gcc-base/plan.sh
+++ packages/bootstrap/build-tools/cross-compiler/native-cross-gcc-base/plan.sh
@@ -1,10 +1,10 @@
 program="gcc"
 native_target="${TARGET_ARCH:-${pkg_target%%-*}}-hab-linux-gnu"
-glibc_version="2.36"
+glibc_version="2.34"
 
 pkg_name="native-cross-gcc-base"
 pkg_origin="core"
-pkg_version="12.2.0"
+pkg_version="9.4.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 The GNU Compiler Collection (GCC) is a compiler system produced by the GNU \
@@ -15,7 +15,7 @@ systems.\
 pkg_upstream_url="https://gcc.gnu.org/"
 pkg_license=('GPL-3.0-or-later WITH GCC-exception-3.1' 'LGPL-3.0-or-later')
 pkg_source="http://ftp.gnu.org/gnu/$program/${program}-${pkg_version}/${program}-${pkg_version}.tar.xz"
-pkg_shasum="e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff"
+pkg_shasum="c95da32f440378d7751dd95533186f7fc05ceb4fb65eb5b85234e6299eb9838e"
 pkg_dirname="${program}-${pkg_version}"
 
 pkg_deps=(
@@ -31,6 +31,7 @@ pkg_deps=(
 pkg_lib_dirs=(lib)
 
 do_prepare() {
+	echo `pwd`
 	# Tell gcc not to look under the default `/lib/` and `/usr/lib/` directories
 	# for libraries
 	#
@@ -94,7 +95,7 @@ do_install() {
 	cat ../gcc/limitx.h ../gcc/glimits.h ../gcc/limity.h >"$(dirname "$("$pkg_prefix"/bin/"$native_target"-gcc -print-libgcc-file-name)")"/install-tools/include/limits.h
 
 	# Install the full limits.h file
-	"$pkg_prefix"/libexec/gcc/"$native_target"/12.2.0/install-tools/mkheaders
+	"$pkg_prefix"/libexec/gcc/"$native_target"/9.4.0/install-tools/mkheaders
 
 	# Remove unnecesary include folder created by 'make install'
 	rm -rf "$pkg_prefix/include"
