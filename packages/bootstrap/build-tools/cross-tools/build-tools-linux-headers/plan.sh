4fe8469d708c6dd17679b1eac5e289e3305076e6
raviduddela <rduddela@progress.com>
2023-05-03 11:30:22 +0000

    downgrade native binutils,gcc,linuxheaders
    Signed-off-by: raviduddela <rduddela@progress.com>

--- packages/bootstrap/build-tools/cross-tools/build-tools-linux-headers/plan.sh
+++ packages/bootstrap/build-tools/cross-tools/build-tools-linux-headers/plan.sh
@@ -1,21 +1,32 @@
 pkg_name="build-tools-linux-headers"
 pkg_origin="core"
-pkg_version="5.19.8"
+pkg_version="4.20.17"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The Linux kernel headers"
 pkg_upstream_url="https://kernel.org"
 pkg_license=("GPL-2.0-only WITH Linux-syscall-note")
-pkg_source="https://www.kernel.org/pub/linux/kernel/v5.x/linux-${pkg_version}.tar.xz"
-pkg_shasum="616308795a952a6a39b4c74807c33916850eb7166d8ed7c9a87a1ba55d7487ce"
+pkg_source="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz"
+pkg_shasum="d011245629b980d4c15febf080b54804aaf215167b514a3577feddb2495f8a3e"
 pkg_dirname="linux-$pkg_version"
 
 pkg_include_dirs=(include)
 
 do_build() {
-	make headers
+	ARCH=`uname -m`
+	case $ARCH in 
+		x86_x64)
+			make headers_install ARCH=x86 INSTALL_HDR_PATH="$pkg_prefix"
+			;;
+		aarch64)
+			make headers_install ARCH=arm64 INSTALL_HDR_PATH="$pkg_prefix"
+			;;
+
+	esac
 }
 
 do_install() {
-	find usr/include -type f ! -name '*.h' -delete
-	cp -rv usr/include "$pkg_prefix"
+	find "$pkg_prefix/include" \
+    	\( -name ..install.cmd -o -name .install \) \
+    	-print0 \
+  	| xargs -0 rm -v
 }
