5c476bd1f37efdad63e3358b96c04118eab6774e
raviduddela <rduddela@progress.com>
2023-05-09 08:36:34 +0000

    downgrade buildtools grep,gcc,gzip,xz,file,busybox-static,binutils
    Signed-off-by: raviduddela <rduddela@progress.com>

--- packages/bootstrap/build-tools/cross-tools/build-tools-file/plan.sh
+++ packages/bootstrap/build-tools/cross-tools/build-tools-file/plan.sh
@@ -3,7 +3,7 @@ native_target="${TARGET_ARCH:-${pkg_target%%-*}}-hab-linux-gnu"
 
 pkg_name="build-tools-file"
 pkg_origin="core"
-pkg_version="5.42"
+pkg_version="5.41"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 file is a standard Unix program for recognizing the type of data contained in \
@@ -12,7 +12,7 @@ a computer file.\
 pkg_upstream_url="https://www.darwinsys.com/file/"
 pkg_license=("LicenseRef-file")
 pkg_source="ftp://ftp.astron.com/pub/${program}/${program}-${pkg_version}.tar.gz"
-pkg_shasum="c076fb4d029c74073f15c43361ef572cfb868407d347190ba834af3b1639b0e4"
+pkg_shasum="13e532c7b364f7d57e23dfeea3147103150cb90593a57af86c10e4f6e411603f"
 pkg_dirname="${program}-${pkg_version}"
 pkg_deps=(
 	core/build-tools-glibc
