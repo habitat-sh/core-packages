f5e9923241a2980679898fe5449fc7e075a65886
raviduddela <rduddela@progress.com>
2023-08-23 06:04:17 +0000

    move plans from core-plans repo,chef4298
    Signed-off-by: raviduddela <rduddela@progress.com>

---
+++ packages/development/libraries/cairo/plan.sh
@@ -0,0 +1,76 @@
+pkg_name=cairo
+pkg_origin=core
+pkg_version="1.16.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=(
+	"LGPL-2.1"
+	"MPL-1.1"
+)
+pkg_source="https://www.cairographics.org/releases/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="5e7b29b3f113ef870d1e3ecf8adf21f923396401604bda16d44be45e66052331"
+pkg_description="Cairo is a 2D graphics library with support for multiple output devices."
+pkg_upstream_url="https://www.cairographics.org"
+pkg_deps=(
+	core/bzip2
+	core/expat
+	core/fontconfig
+	core/freetype
+	core/gcc-libs
+	core/glib
+	core/glibc
+	core/libffi
+	core/libice
+	core/libiconv
+	core/libpng
+	core/libsm
+	core/libxau
+	core/libxcb
+	core/libxdmcp
+	core/libxext
+	core/lzo
+	core/pcre
+	core/pixman
+	core/xlib
+	core/zlib
+)
+pkg_build_deps=(
+	core/diffutils
+	core/file
+	core/gcc
+	core/make
+	core/pkg-config
+	core/xextproto
+	core/xproto
+)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(
+	lib
+	lib/cairo
+)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_prepare() {
+	if [[ ! -r /usr/bin/file ]]; then
+		ln -sv "$(pkg_path_for file)/bin/file" /usr/bin/file
+		_clean_file=true
+	fi
+}
+
+do_build() {
+	CFLAGS="-Os ${CFLAGS}"
+
+	./configure --prefix="${pkg_prefix}" \
+		--enable-xlib
+	make
+}
+
+do_check() {
+	make test
+}
+
+do_end() {
+	if [[ -n $_clean_file ]]; then
+		rm -fv /usr/bin/file
+	fi
+}
---
+++ packages/development/tools/misc/ccache/plan.sh
@@ -0,0 +1,17 @@
+pkg_origin=core
+pkg_name=ccache
+pkg_version=3.7.12
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('GPL-3.0')
+pkg_description="ccache is a compiler cache. It speeds up recompilation by caching previous compilations and ""\
+detecting when the same compilation is being done again."
+pkg_upstream_url=https://ccache.samba.org/
+pkg_source=https://github.com/ccache/ccache/releases/download/v${pkg_version}/ccache-${pkg_version}.tar.gz
+pkg_shasum=d2abe88d4c283ce960e233583061127b156ffb027c6da3cf10770fc0c7244194
+pkg_deps=(core/glibc core/zlib)
+pkg_build_deps=(core/gcc core/make core/diffutils core/which core/perl)
+pkg_bin_dirs=(bin)
+
+do_check() {
+	make test
+}
