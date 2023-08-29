138a7b254914fe9eda70d36343e22a3af4c6e03e
raviduddela <rduddela@progress.com>
2023-08-29 08:21:35 +0000

    move plans from core-plans repo,chef4302
    Signed-off-by: raviduddela <rduddela@progress.com>

---
+++ packages/development/libraries/dbus/plan.sh
@@ -0,0 +1,20 @@
+pkg_name=dbus
+pkg_origin=core
+pkg_version="1.13.18"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('GPLv2')
+pkg_description="D-Bus is a message bus system, a simple way for applications to talk to one another."
+pkg_upstream_url="https://www.freedesktop.org/wiki/Software/dbus/"
+pkg_source="https://dbus.freedesktop.org/releases/dbus/${pkg_name}-${pkg_version}.tar.xz"
+pkg_shasum="8078f5c25e34ab907ce06905d969dc8ef0ccbec367e1e1707c7ecf8460f4254e"
+pkg_deps=(core/glibc)
+pkg_build_deps=(
+	core/autoconf
+	core/automake
+	core/make
+	core/expat
+	core/gcc
+	core/pkg-config
+)
+pkg_lib_dirs=(lib)
+pkg_bin_dirs=(bin)
