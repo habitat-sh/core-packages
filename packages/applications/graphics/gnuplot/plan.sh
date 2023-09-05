4a66c014c484d97d3ca84312b0a84c325cfc3270
Nimit <nimit.jyotiana@hotmail.com>
2023-09-05 08:17:25 +0000

    migrating gnuplot
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

---
+++ packages/applications/graphics/gnuplot/plan.sh
@@ -0,0 +1,24 @@
+pkg_name="gnuplot"
+pkg_origin="core"
+pkg_version="5.4.2"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_upstream_url="http://www.gnuplot.info"
+pkg_description="Gnuplot is a portable command-line driven graphing
+utility for Linux, OS/2, MS Windows, OSX, VMS, and many other
+platforms"
+pkg_license=('gnuplot')
+pkg_source="https://sourceforge.net/projects/$pkg_name/files/$pkg_name/$pkg_version/$pkg_name-$pkg_version.tar.gz"
+pkg_shasum="e57c75e1318133951d32a83bcdc4aff17fed28722c4e71f2305cfc2ae1cae7ba"
+pkg_deps=(
+	core/gcc-libs
+)
+pkg_build_deps=(
+  core/gcc
+  core/make
+  core/pkg-config
+)
+pkg_bin_dirs=(bin)
+
+do_check() {
+  make check
+}
