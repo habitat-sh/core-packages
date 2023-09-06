1459e664ef891fe997632e1ca05489be885e4b7e
Sangameshwar Mandakanalli <Sangameshwar.mandakanalli@progress.com>
2023-09-06 12:01:02 +0000

    Moving packages from core-plans to core-packages[chef-4595]
    Signed-off-by: Sangameshwar Mandakanalli <Sangameshwar.mandakanalli@progress.com>

---
+++ packages/development/libraries/libksba/plan.sh
@@ -0,0 +1,48 @@
+pkg_name=libksba
+pkg_origin=core
+pkg_version=1.6.0
+pkg_license=('LGPL-3.0-or-later')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source=ftp://ftp.gnupg.org/gcrypt/"${pkg_name}"/"${pkg_name}"-"${pkg_version}".tar.bz2
+pkg_upstream_url="https://www.gnupg.org/software/libksba/index.html"
+pkg_description="Libksba is a library to make the tasks of working with X.509 certificates, CMS data and related objects more easy."
+pkg_shasum=dad683e6f2d915d880aa4bed5cea9a115690b8935b78a1bbe01669189307a48b
+pkg_deps=(
+  core/glibc
+  core/libgpg-error
+)
+pkg_build_deps=(
+  core/gcc
+  core/coreutils
+  core/sed
+  core/bison
+  core/flex
+  core/grep
+  core/bash
+  core/gawk
+  core/libtool
+  core/diffutils
+  core/findutils
+  core/xz
+  core/gettext
+  core/gzip
+  core/make
+  core/patch
+  core/texinfo
+  core/util-linux
+)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+
+do_build() {
+  ./configure \
+    --prefix="${pkg_prefix}" \
+    --enable-static \
+    --enable-shared
+  make
+}
+
+do_check() {
+  make check
+}
---
+++ packages/development/libraries/libnsl/plan.sh
@@ -0,0 +1,37 @@
+pkg_name=libnsl
+pkg_origin=core
+pkg_version="1.3.0"
+pkg_description="This library contains the public client interface for NIS(YP) and NIS+ in a IPv6 ready version"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("LGPL-2.1-or-later")
+pkg_upstream_url="https://github.com/thkukuk/libnsl"
+pkg_source="https://github.com/thkukuk/libnsl/archive/v${pkg_version}.tar.gz"
+pkg_shasum=8e88017f01dd428f50386186b0cd82ad06c9b2a47f9c5ea6b3023fc6e08a6b0f
+pkg_deps=(
+  core/glibc
+  core/krb5
+  core/libtirpc
+)
+pkg_build_deps=(
+  core/autoconf
+  core/automake
+  core/diffutils
+  core/gcc
+  core/gettext
+  core/libtool
+  core/make
+  core/patch
+  core/pkg-config
+)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_prepare() {
+  ACLOCAL_PATH="${ACLOCAL_PATH}:$(pkg_path_for core/gettext)/share/aclocal:$(pkg_path_for core/pkg-config)/share/aclocal:$(pkg_path_for core/libtool)/share/aclocal"
+  export ACLOCAL_PATH
+
+  autoreconf --force --install
+  patch < "$PLAN_CONTEXT/autogen.patch"
+  ./autogen.sh
+}
