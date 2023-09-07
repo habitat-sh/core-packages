5bdfc1e6e6a8663ff2f3a39dd347a0171114ee6c
raviduddela <rduddela@progress.com>
2023-09-07 08:19:55 +0000

    move plans from core-plans repo,chef4637
    Signed-off-by: raviduddela <rduddela@progress.com>

---
+++ packages/applications/servers/monitoring/telegraf/plan.sh
@@ -0,0 +1,24 @@
+pkg_name=telegraf
+pkg_origin=core
+pkg_version=1.20.3
+pkg_license=('MIT')
+pkg_description="telegraf - client for InfluxDB"
+pkg_upstream_url="https://github.com/influxdata/telegraf/"
+pkg_source="https://dl.influxdata.com/${pkg_name}/releases/${pkg_name}-${pkg_version}_static_linux_amd64.tar.gz"
+pkg_shasum=3f245da927c2e3289afdc8e70bb93ad495ebda2f573db5fcea16b1bbcd45ebd7
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_svc_run="telegraf --config ${pkg_svc_config_path}/telegraf.conf"
+pkg_build_deps=(
+	core/wget
+	core/tar
+)
+pkg_deps=()
+pkg_bin_dirs=(bin)
+
+do_build() {
+	return 0
+}
+
+do_install() {
+	install -vD "${HAB_CACHE_SRC_PATH}/${pkg_name}-${pkg_version}/usr/bin/telegraf" "${pkg_prefix}/bin/telegraf"
+}
---
+++ packages/tools/networking/tcpdump/plan.sh
@@ -0,0 +1,36 @@
+pkg_name=tcpdump
+pkg_origin=core
+pkg_version=4.99.1
+pkg_description="A powerful command-line packet analyzer."
+pkg_upstream_url="http://www.tcpdump.org/"
+pkg_license=('BSD')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="http://www.tcpdump.org/release/tcpdump-${pkg_version}.tar.gz"
+pkg_shasum=79b36985fb2703146618d87c4acde3e068b91c553fb93f021a337f175fd10ebe
+# core/coreutils isn't /really/ needed at runtime, but fix_interpreter
+# only works if the dep is listed in pkg_deps
+pkg_deps=(
+	core/glibc
+	core/libpcap
+	core/openssl
+	core/coreutils
+)
+pkg_build_deps=(
+	core/gcc
+	core/make
+	core/perl
+	core/diffutils
+)
+pkg_bin_dirs=(bin)
+
+do_build() {
+	./configure \
+		--prefix="${pkg_prefix}" \
+		--with-crypto
+	make -j "$(nproc)"
+}
+
+do_check() {
+	fix_interpreter "tests/TESTrun" core/coreutils bin/env
+	make check
+}
