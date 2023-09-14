f1ccfd989508800d3179ca63c313ef610a842036
Nimit <nimit.jyotiana@hotmail.com>
2023-09-14 05:10:32 +0000

    migrating rust
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

---
+++ packages/development/compilers/rust/glibc/x86_64-windows/plan.ps1
@@ -0,0 +1,29 @@
+$pkg_name="rust"
+$pkg_origin="core"
+$pkg_version="1.68.2"
+$pkg_description="Safe, concurrent, practical language"
+$pkg_upstream_url="https://www.rust-lang.org/"
+$pkg_license=@("Apache-2.0", "MIT")
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_source="https://static.rust-lang.org/dist/rust-$pkg_version-x86_64-pc-windows-msvc.msi"
+$pkg_shasum = "4ac23f74d98809036b686b80ff856a125b0a4009b3fef96290b540cbef382489"
+$pkg_deps=@("core/visual-cpp-redist-2015", "core/visual-cpp-build-tools-2015")
+$pkg_build_deps=@("core/lessmsi")
+$pkg_bin_dirs=@("bin")
+$pkg_lib_dirs=@("lib")
+
+function Invoke-Unpack {
+    mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+    Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+    try {
+        lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH/$pkg_filename").Path
+    } finally { Pop-Location }
+}
+
+function Invoke-Install {
+    Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/rust-$pkg_version-x86_64-pc-windows-msvc/SourceDir/Rust/*" "$pkg_prefix" -Recurse -Force
+}
+
+function Invoke-Check() {
+    (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/rust-$pkg_version-x86_64-pc-windows-msvc/SourceDir/Rust/bin/rustc.exe" --version).StartsWith("rustc $pkg_version")
+}
