b9f6e3d4f1c7804740c439f97c6dc2093e55efea
raviduddela <rduddela@progress.com>
2023-09-22 08:54:07 +0000

    move plans from core-plans repo,chef4612
    Signed-off-by: raviduddela <rduddela@progress.com>

---
+++ packages/development/compilers/ocaml/plan.sh
@@ -0,0 +1,25 @@
+pkg_name=ocaml
+pkg_origin=core
+pkg_version="4.13.1"
+pkg_description="The OCAML compiler"
+pkg_upstream_url="https://ocaml.org/"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('LGPL-2.0')
+pkg_source="https://github.com/ocaml/ocaml/archive/${pkg_version}.tar.gz"
+pkg_shasum="194c7988cc1fd1c64f53f32f2f7551e5309e44d914d6efc7e2e4d002296aeac4"
+pkg_deps=(core/glibc)
+pkg_build_deps=(core/make core/gcc)
+pkg_lib_dirs=(lib)
+pkg_bin_dirs=(bin)
+
+do_build() {
+	./configure --prefix "${pkg_prefix}" && make world.opt
+}
+
+do_check() {
+	make bootstrap
+}
+
+do_strip() {
+	return 0
+}
---
+++ packages/development/tools/ocaml/ocamlbuild/plan.sh
@@ -0,0 +1,25 @@
+pkg_name=ocamlbuild
+pkg_origin=core
+pkg_version="0.14.0"
+pkg_description="OCamlbuild is a generic build tool, that has built-in rules for building OCaml library and programs."
+pkg_upstream_url="https://github.com/ocaml/ocamlbuild"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('LGPL')
+pkg_source="https://github.com/ocaml/ocamlbuild/archive/${pkg_version}.tar.gz"
+pkg_shasum=87b29ce96958096c0a1a8eeafeb6268077b2d11e1bf2b3de0f5ebc9cf8d42e78
+pkg_deps=(
+	core/glibc
+	core/ocaml
+	core/coreutils
+	core/ncurses
+)
+pkg_build_deps=(
+	core/gcc
+	core/make
+)
+pkg_bin_dirs=(bin)
+
+do_build() {
+	make configure OCAMLBUILD_PREFIX="${pkg_prefix}" OCAMLBUILD_BINDIR="${pkg_prefix}/bin" OCAMLBUILD_LIBDIR="${pkg_prefix}/lib"
+	make
+}
