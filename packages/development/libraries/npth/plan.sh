349badd902fc8a48ece3e2378b267e3943ff6574
raviduddela <rduddela@progress.com>
2023-09-28 12:17:38 +0000

    move plans from core-plans repo,chef4611
    Signed-off-by: raviduddela <rduddela@progress.com>

---
+++ packages/development/libraries/npth/plan.sh
@@ -0,0 +1,45 @@
+pkg_name=npth
+pkg_origin=core
+pkg_version=1.6
+pkg_license=('LGPL-3.0-or-later')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source=ftp://ftp.gnupg.org/gcrypt/"${pkg_name}"/"${pkg_name}"-"${pkg_version}".tar.bz2
+pkg_upstream_url="https://gnupg.org/software/npth/index.html"
+pkg_description="nPth is a library to provide the GNU Pth API and thus a non-preemptive threads implementation."
+pkg_shasum=1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1
+pkg_deps=(core/glibc)
+pkg_build_deps=(
+	core/gcc
+	core/coreutils
+	core/sed
+	core/bison
+	core/flex
+	core/grep
+	core/bash
+	core/gawk
+	core/libtool
+	core/diffutils
+	core/findutils
+	core/xz
+	core/gettext
+	core/gzip
+	core/make
+	core/patch
+	core/texinfo
+	core/util-linux
+)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+
+do_build() {
+	./configure \
+		--prefix="${pkg_prefix}" \
+		--enable-static \
+		--enable-shared
+	make
+}
+
+do_check() {
+	make check
+}
---
+++ packages/development/libraries/nspr/README.md
@@ -0,0 +1,18 @@
+# nspr
+
+Netscape Portable Runtime
+
+## Maintainers
+
+* The Habitat Maintainers: <humans@habitat.sh>
+
+## Type of Package
+
+Binary package
+
+## Usage
+
+Typically this is a runtime dependency that can be added to your
+plan.sh:
+
+    pkg_deps=(core/nspr)
