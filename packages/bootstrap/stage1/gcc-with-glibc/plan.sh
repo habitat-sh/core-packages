dab6764c2dd40403c5f7e5bb09fab3fa23b9b5f1
Ravi Duddela <RaviShankar.Duddela@progress.com>
2024-03-19 07:54:32 +0530

    minor change
    Signed-off-by: Ravi Duddela <RaviShankar.Duddela@progress.com>

--- packages/bootstrap/stage1/gcc-with-glibc/plan.sh
+++ packages/bootstrap/stage1/gcc-with-glibc/plan.sh
@@ -2,7 +2,7 @@ program="gcc"
 
 pkg_name="gcc-stage1-with-glibc"
 pkg_origin="core"
-pkg_version="9.4.0"
+pkg_version="12.2.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 This is wrapper package that ensures gcc-stage1 uses the core/glibc as the
