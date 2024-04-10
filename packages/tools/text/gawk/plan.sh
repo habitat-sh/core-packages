536036984c50348a22ac88ff325fc39146e4f377
Nimit <nimit.jyotiana@hotmail.com>
2024-04-10 15:02:20 +0000

    [gawk] upgraded to v5.3.0
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- packages/tools/text/gawk/plan.sh
+++ packages/tools/text/gawk/plan.sh
@@ -2,7 +2,7 @@ program="gawk"
 
 pkg_name="gawk"
 pkg_origin="core"
-pkg_version="5.2.0"
+pkg_version="5.3.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 The awk utility interprets a special-purpose programming language that makes \
@@ -10,9 +10,9 @@ it possible to handle simple data-reformatting jobs with just a few lines of \
 code.\
 "
 pkg_upstream_url="http://www.gnu.org/software/gawk/"
-pkg_license=('GPL-3.0-or-later')
+pkg_license=('GPL-3.0-only' 'LGPL-2.0-only')
 pkg_source="http://ftp.gnu.org/gnu/${program}/${program}-${pkg_version}.tar.gz"
-pkg_shasum="ef5af4449cb0269faf3af24bf4c02273d455f0741bf3c50f86ddc09332d6cf56"
+pkg_shasum="378f8864ec21cfceaa048f7e1869ac9b4597b449087caf1eb55e440d30273336"
 pkg_dirname="${program}-${pkg_version}"
 
 pkg_deps=(
@@ -26,17 +26,23 @@ pkg_build_deps=(
 	core/gcc
 )
 pkg_bin_dirs=(bin)
-pkg_interpreters=(bin/awk bin/gawk)
+#pkg_interpreters=(bin/awk bin/gawk)
 
-do_prepare() {
-	# Prevent building unnecessary binaries
-	sed -i 's/extras//' Makefile.in
-	# Fix a bug from upstream: https://clfs.org/~kb0iic/lfs-systemd/chapter08/gawk.html
-	sed -i '241i UPREF(m);' interpret.h
+do_build() {
+	./configure --prefix="$pkg_prefix"
+	make
 }
 
 do_check() {
 	# This currently passes in core-plans CI but may fail on some workstations.
 	# Ref: https://github.com/habitat-sh/core-plans/issues/2879
 	make check
+}
+
+do_install() {
+	do_default_install
+
+	# copy license files to package
+	install -Dm644 ${CACHE_PATH}/COPYING ${pkg_prefix}
+	install -Dm644 ${CACHE_PATH}/missing_d/COPYING.LIB ${pkg_prefix}
 }
