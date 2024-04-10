22be9507359c9a8c98d7892b3fc20358861eb83d
Nimit <nimit.jyotiana@hotmail.com>
2024-04-10 15:53:08 +0000

    [gzip] upgraded to v1.13
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- packages/tools/compression/gzip/plan.sh
+++ packages/tools/compression/gzip/plan.sh
@@ -2,16 +2,16 @@ program="gzip"
 
 pkg_name="gzip"
 pkg_origin="core"
-pkg_version="1.12"
+pkg_version="1.13"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 GNU Gzip is a popular data compression program originally written by Jean-loup \
 Gailly for the GNU project.\
 "
 pkg_upstream_url="https://www.gnu.org/software/gzip/"
-pkg_license=('GPL-3.0-or-later')
+pkg_license=("GPL-3.0-only")
 pkg_source="http://ftp.gnu.org/gnu/${program}/${program}-${pkg_version}.tar.xz"
-pkg_shasum="ce5e03e519f637e1f814011ace35c4f87b33c0bbabeec35baf5fbd3479e91956"
+pkg_shasum="7454eb6935db17c6655576c2e1b0fabefd38b4d0936e0f87f48cd062ce91a057"
 
 pkg_deps=(
 	core/coreutils
@@ -40,4 +40,6 @@ do_check() {
 
 do_install() {
 	make install
+
+	install -Dm644 ${CACHE_PATH}/COPYING ${pkg_prefix}
 }
