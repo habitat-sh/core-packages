c142feb43b0f2bf3bb9cc5d37134f78f78c2891c
Sangameshwar <Sangameshwar.mandakanalli@progress.com>
2024-04-10 19:30:30 +0530

    zlib bumped to 1.3.1 [CHEF-11545]
    Signed-off-by: Sangameshwar <Sangameshwar.mandakanalli@progress.com>

--- packages/bootstrap/stage1/zlib/plan.sh
+++ packages/bootstrap/stage1/zlib/plan.sh
@@ -2,7 +2,7 @@ program="zlib"
 
 pkg_name="zlib-stage1"
 pkg_origin="core"
-pkg_version="1.2.13"
+pkg_version="1.3.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Compression library implementing the deflate compression method found in gzip \
@@ -11,7 +11,7 @@ and PKZIP.\
 pkg_upstream_url="http://www.zlib.net/"
 pkg_license=('Zlib')
 pkg_source="http://zlib.net/${program}-${pkg_version}.tar.gz"
-pkg_shasum="b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
+pkg_shasum="6931283d9ac87c5073f30b6290c4c75f21632bb4fc3603ac8100812bed248159"
 pkg_dirname="${program}-${pkg_version}"
 
 pkg_build_deps=(
