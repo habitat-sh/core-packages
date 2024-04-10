6f9556145f560102998e80c4bba5a31e85180a44
Nimit <nimit.jyotiana@hotmail.com>
2024-04-10 15:12:04 +0000

    [sed] upgraded to v4.9
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- packages/tools/text/sed/plan.sh
+++ packages/tools/text/sed/plan.sh
@@ -1,6 +1,6 @@
 pkg_name="sed"
 pkg_origin="core"
-pkg_version="4.8"
+pkg_version="4.9"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 sed (stream editor) is a non-interactive command-line text editor. sed is \
@@ -10,9 +10,9 @@ typically used for extracting part of a file using pattern matching or \
 substituting multiple occurrences of a string within a file.\
 "
 pkg_upstream_url="https://www.gnu.org/software/sed/"
-pkg_license=("GPL-3.0-or-later")
+pkg_license=("GPL-3.0-only")
 pkg_source="http://ftp.gnu.org/gnu/$pkg_name/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum="f79b0cfea71b37a8eeec8490db6c5f7ae7719c35587f21edb0617f370eeff633"
+pkg_shasum="6e226b732e1cd739464ad6862bd1a1aba42d7982922da7a53519631d24975181"
 pkg_deps=(
 	core/acl
 	core/glibc
@@ -27,4 +27,11 @@ pkg_bin_dirs=(bin)
 do_check() {
 	chown -R hab .
 	su hab -c "PATH=$PATH make check"
+}
+
+do_install() {
+	do_default_install
+
+	# copy license files to package
+	install -Dm644 ${CACHE_PATH}/COPYING ${pkg_prefix}
 }
