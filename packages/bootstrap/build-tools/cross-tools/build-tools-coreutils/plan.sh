b4d6e094b92b794703d1bade2113b5faeb9947ea
Nimit <nimit.jyotiana@hotmail.com>
2024-04-15 02:08:41 +0000

    [coreutils] upgraded to v9.4
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- packages/bootstrap/build-tools/cross-tools/build-tools-coreutils/plan.sh
+++ packages/bootstrap/build-tools/cross-tools/build-tools-coreutils/plan.sh
@@ -3,7 +3,7 @@ native_target="${TARGET_ARCH:-${pkg_target%%-*}}-hab-linux-gnu"
 
 pkg_name="build-tools-coreutils"
 pkg_origin="core"
-pkg_version="9.1"
+pkg_version="9.4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 The GNU Core Utilities are the basic file, shell and text manipulation \
@@ -13,7 +13,7 @@ expected to exist on every operating system.\
 pkg_upstream_url="https://www.gnu.org/software/coreutils/"
 pkg_license=("GPL-3.0-or-later")
 pkg_source="http://ftp.gnu.org/gnu/$program/${program}-${pkg_version}.tar.xz"
-pkg_shasum="61a1f410d78ba7e7f37a5a4f50e6d1320aca33375484a3255eddf17a38580423"
+pkg_shasum="ea613a4cf44612326e917201bbbcdfbd301de21ffc3b59b6e5c07e040b275e52"
 pkg_dirname="${program}-${pkg_version}"
 pkg_deps=(
 	core/build-tools-glibc
@@ -32,4 +32,10 @@ do_build() {
 		--enable-install-program=hostname \
 		--enable-no-install-program=kill,uptime
 	make
+}
+
+do_install() {
+	do_default_install
+
+	install -Dm644 ${CACHE_PATH}/COPYING ${pkg_prefix}
 }
--- packages/bootstrap/stage1/coreutils/plan.sh
+++ packages/bootstrap/stage1/coreutils/plan.sh
@@ -2,7 +2,7 @@ program="coreutils"
 
 pkg_name="coreutils-stage1"
 pkg_origin="core"
-pkg_version="9.1"
+pkg_version="9.4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 The GNU Core Utilities are the basic file, shell and text manipulation \
@@ -12,7 +12,7 @@ expected to exist on every operating system.\
 pkg_upstream_url="https://www.gnu.org/software/coreutils/"
 pkg_license=("GPL-3.0-or-later")
 pkg_source="http://ftp.gnu.org/gnu/$program/${program}-${pkg_version}.tar.xz"
-pkg_shasum="61a1f410d78ba7e7f37a5a4f50e6d1320aca33375484a3255eddf17a38580423"
+pkg_shasum="ea613a4cf44612326e917201bbbcdfbd301de21ffc3b59b6e5c07e040b275e52"
 pkg_dirname="${program}-${pkg_version}"
 
 pkg_deps=(
@@ -45,4 +45,10 @@ do_check() {
 	sed -e "s^#\!.*bin/python^#\!$(pkg_path_for build-tools-python)/bin/python^" -i tests/d_type-check
 	make NON_ROOT_USERNAME=nobody check-root
 	make RUN_EXPENSIVE_TESTS=yes check
+}
+
+do_install() {
+	do_default_install
+
+	install -Dm644 ${CACHE_PATH}/COPYING ${pkg_prefix}
 }
