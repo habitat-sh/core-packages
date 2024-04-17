4dd634fc5d32d6115268944d8659b6976464517e
Ravi Duddela <rduddela@progress.com>
2024-04-17 06:18:50 +0000

    update libedit to 20230828-3.1
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- packages/development/libraries/libedit/plan.sh
+++ packages/development/libraries/libedit/plan.sh
@@ -1,12 +1,12 @@
 pkg_name="libedit"
 pkg_origin="core"
-pkg_version="20221030-3.1"
+pkg_version="20230828-3.1"
 pkg_license=('BSD-3-Clause')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="http://thrysoee.dk/editline/${pkg_name}-${pkg_version}.tar.gz"
 pkg_upstream_url="https://thrysoee.dk/editline/"
 pkg_description="This is an autotool- and libtoolized port of the NetBSD Editline library (libedit)."
-pkg_shasum="f0925a5adf4b1bf116ee19766b7daa766917aec198747943b1c4edf67a4be2bb"
+pkg_shasum="4ee8182b6e569290e7d1f44f0f78dac8716b35f656b76528f699c69c98814dad"
 pkg_deps=(
 	core/glibc
 	core/ncurses
