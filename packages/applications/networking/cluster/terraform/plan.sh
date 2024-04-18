da96513958fbe9e1369ca93a8749c62f531f4ae4
Sangameshwar <Sangameshwar.mandakanalli@progress.com>
2024-04-18 19:22:36 +0530

    Adding terraform with version 1.7.3 [CHEF-11661]
    Signed-off-by: Sangameshwar <Sangameshwar.mandakanalli@progress.com>

---
+++ packages/applications/networking/cluster/terraform/plan.sh
@@ -0,0 +1,28 @@
+pkg_name=terraform
+pkg_origin=core
+pkg_version=1.7.3
+pkg_license=('MPL-2.0')
+pkg_description="Terraform is a tool for building, changing, and combining infrastructure safely and efficiently"
+pkg_upstream_url="http://www.terraform.io/"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="https://releases.hashicorp.com/${pkg_name}/${pkg_version}/${pkg_name}_${pkg_version}_linux_amd64.zip"
+pkg_filename="${pkg_name}_${pkg_version}_linux_amd64.zip"
+pkg_shasum=617042989ce46b5dd07772237b49b57b8f8e97b1604c9dbbd85ead87effb51fe
+pkg_build_deps=(core/unzip)
+pkg_deps=()
+pkg_bin_dirs=(bin)
+
+# The pkg_filename does not extract into a folder. We need to force it.
+do_unpack() {
+  cd "${HAB_CACHE_SRC_PATH}" || exit
+  unzip "${pkg_filename}" -d "${pkg_dirname}"
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  install -D terraform "${pkg_prefix}/bin/terraform"
+}
+
