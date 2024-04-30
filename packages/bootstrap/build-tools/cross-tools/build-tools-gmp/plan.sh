46ea4391dcd4e8c75a2190f23ae8e19e31a100ac
Phani Sajja <psajja@progress.com>
2024-04-30 16:39:14 +0530

    fix build-tools-gmp shasum
    Signed-off-by: Phani Sajja <psajja@progress.com>

--- packages/bootstrap/build-tools/cross-tools/build-tools-gmp/plan.sh
+++ packages/bootstrap/build-tools/cross-tools/build-tools-gmp/plan.sh
@@ -12,7 +12,7 @@ integers, rational numbers, and floating-point numbers.\
 pkg_upstream_url="https://gmplib.org"
 pkg_license=('LGPL-3.0-or-later')
 pkg_source="http://ftp.gnu.org/gnu/${program}/${program}-${pkg_version}.tar.xz"
-pkg_shasum="fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2"
+pkg_shasum="a3c2b80201b89e68616f4ad30bc66aee4927c3ce50e33929ca819d5c43538898"
 pkg_dirname="${program}-${pkg_version}"
 pkg_include_dirs=(include)
 pkg_lib_dirs=(lib)
