ed4d2d17fa7d98ddbefcb583a14c684b162e9378
Ravi Duddela <rduddela@progress.com>
2024-05-07 06:29:54 +0000

    change ruby name as per latest rule
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- packages/development/interpreters/ruby/3_1/plan.sh
+++ packages/development/interpreters/ruby/3_1/plan.sh
@@ -1,7 +1,7 @@
-pkg_name=ruby33
+pkg_name=ruby3_1
 pkg_origin=core
-pkg_major=3.3
-pkg_version=3.3.0
+pkg_major=3.1
+pkg_version=3.1.3
 pkg_description="A dynamic, open source programming language with a focus on \
   simplicity and productivity. It has an elegant syntax that is natural to \
   read and easy to write."
@@ -9,7 +9,7 @@ pkg_license=("Ruby")
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://cache.ruby-lang.org/pub/ruby/${pkg_major}/ruby-${pkg_version}.tar.gz
 pkg_upstream_url=https://www.ruby-lang.org/en/
-pkg_shasum="96518814d9832bece92a85415a819d4893b307db5921ae1f0f751a9a89a56b7d"
+pkg_shasum=5ea498a35f4cd15875200a52dde42b6eb179e1264e17d78732c3a57cd1c6ab9e
 pkg_deps=(
 	core/bash
 	core/coreutils
@@ -21,12 +21,10 @@ pkg_deps=(
 	core/nss-myhostname
 	core/openssl
 	core/zlib
-	
 )
 pkg_build_deps=(
 	core/gcc
 	core/git
-	core/ruby-stage1
 )
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
@@ -41,6 +39,7 @@ do_prepare() {
 	grep -lr '/usr/bin/env' . | while read -r f; do
 		sed -e "s,/usr/bin/env,$(pkg_interpreter_for coreutils bin/env),g" -i "$f"
 	done
+
 	build_line "Setting CFLAGS='$CFLAGS'"
 }
 
@@ -53,7 +52,7 @@ do_build() {
 		--disable-install-doc \
 		--with-openssl-dir="$(pkg_path_for core/openssl)" \
 		--with-libyaml-dir="$(pkg_path_for core/libyaml)" \
-		--with-baseruby="$(pkg_path_for core/ruby-stage1)/bin/ruby"
+		--without-baseruby
 
 	make -j"$(nproc)"
 	popd || exit 1
