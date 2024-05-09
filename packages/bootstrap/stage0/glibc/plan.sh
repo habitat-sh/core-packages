2c8a69d8dfb6b2e929b7d6e247309544d749ac7e
Ravi Duddela <rduddela@progress.com>
2024-05-09 11:42:07 +0000

    fix glibc link issue
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- packages/bootstrap/stage0/glibc/plan.sh
+++ packages/bootstrap/stage0/glibc/plan.sh
@@ -113,6 +113,11 @@ do_install() {
 	touch "$pkg_prefix/etc/ld.so.conf"
 
 	make install
+	# Add a `lib64` -> `lib` symlink for `bin/ldd` to work correctly.
+	#
+	# Thanks to: https://github.com/NixOS/nixpkgs/blob/55b03266cfc25ae019af3cdd2cfcad0facdc68f2/pkgs/development/libraries/glibc/builder.sh#L43-L47
+	ln -sv "$pkg_prefix/lib" "$pkg_prefix/lib64"
+
 
 	# Remove unneccessary binaries
 	rm -v "${pkg_prefix}"/bin/{mtrace,sotruss,xtrace,sln}
