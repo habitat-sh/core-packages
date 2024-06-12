3d10e584ce4711c04a009e4223fdf193e0c94d32
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2024-06-12 13:24:12 +0000

    Bumped docker with 26.1.4
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- packages/applications/virtualization/docker/plan.ps1
+++ packages/applications/virtualization/docker/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name = "docker"
 $pkg_description = "The Docker Engine"
 $pkg_origin = "core"
-$pkg_version = "26.1.3"
+$pkg_version = "26.1.4"
 $pkg_maintainer = "The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license = @("Apache-2.0")
 $pkg_source = "https://download.docker.com/win/static/stable/x86_64/$pkg_name-$pkg_version.zip"
 $pkg_upstream_url = "https://docs.docker.com/engine/installation/binaries/"
-$pkg_shasum = "bba77ac3c959387b0047dac25a4268d3312443e208228d4c1b5e5f4715ae152f"
+$pkg_shasum = "381ea87f620e83b5bd6f586465ea5f2f9d5abe539ddc81e6b074a8ff41ffa1cb"
 $pkg_dirname = "docker"
 $pkg_bin_dirs = @("bin")
 
