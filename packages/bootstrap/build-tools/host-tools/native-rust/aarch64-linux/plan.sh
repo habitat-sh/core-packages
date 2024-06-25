90566f4a885b95d29c5304ef79cb81c4e4fe2c32
raviduddela <rduddela@progress.com>
2024-06-25 11:42:01 +0000

    update rust for arm
    Signed-off-by: raviduddela <rduddela@progress.com>

--- packages/bootstrap/build-tools/host-tools/native-rust/aarch64-linux/plan.sh
+++ packages/bootstrap/build-tools/host-tools/native-rust/aarch64-linux/plan.sh
@@ -1,7 +1,7 @@
 program="rust"
 pkg_name="native-rust"
 pkg_origin="core"
-pkg_version="1.68.2"
+pkg_version="1.75.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Rust is a systems programming language that runs blazingly fast, prevents \
@@ -10,7 +10,7 @@ segfaults, and guarantees thread safety.\
 pkg_upstream_url="https://www.rust-lang.org/"
 pkg_license=('Apache-2.0' 'MIT')
 pkg_source="https://static.rust-lang.org/dist/${program}-${pkg_version}-aarch64-unknown-linux-gnu.tar.gz"
-pkg_shasum="1311fa8204f895d054c23a3481de3b158a5cd3b3a6338761fee9cdf4dbf075a5"
+pkg_shasum="30828cd904fcfb47f1ac43627c7033c903889ea4aca538f53dcafbb3744a9a73"
 pkg_dirname="${program}-${pkg_version}-aarch64-unknown-linux-gnu"
 
 pkg_bin_dirs=(bin)
@@ -24,4 +24,8 @@ do_install() {
 	./install.sh --prefix="$pkg_prefix" --disable-ldconfig
 	# Delete the uninstaller script as it is not required
 	rm "${pkg_prefix}"/lib/rustlib/uninstall.sh
+}
+
+do_strip() {
+	return 0
 }
