bd4964ea462d647344c4bd6202d2f0cf36588cd9
Nimit <nimit.jyotiana@hotmail.com>
2024-07-10 05:38:07 +0000

    update ghc and cabal packages
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- packages/bootstrap/stage1/ghc/plan.sh
+++ packages/bootstrap/stage1/ghc/plan.sh
@@ -2,34 +2,91 @@ program="ghc"
 
 pkg_name="ghc-stage1"
 pkg_origin=core
-pkg_version="8.10.7"
-pkg_license=('BSD-3-Clause' 'BSD-2-Clause' 'BSD-Source-Code' 'HaskellReport')
+pkg_version="8.4.4"
+pkg_license=('BSD-2-Clause' 'BSD-3-Clause' 'BSD-Source-Code' 'HaskellReport')
 pkg_upstream_url="https://www.haskell.org/ghc/"
-pkg_description="The Glasgow Haskell Compiler"
+pkg_description="The Glasgow Haskell Compiler - Binary Bootstrap"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source="https://downloads.haskell.org/~ghc/${pkg_version}/ghc-${pkg_version}-x86_64-centos7-linux.tar.xz"
-pkg_shasum="262a50bfb5b7c8770e0d99f54d42e5876968da7bf93e2e4d6cfe397891a36d05"
-pkg_dirname="ghc-${pkg_version}"
+pkg_source="https://downloads.haskell.org/~ghc/${pkg_version}/ghc-${pkg_version}-x86_64-deb9-linux.tar.xz"
+pkg_shasum="47c80a32d8f02838a2401414c94ba260d1fe82b7d090479994522242c767cc83"
+pkg_dirname="ghc-stage1-${pkg_version}"
 
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
-pkg_include_dirs=("lib/ghc-${pkg_version}/include")
-#pkg_interpreters=(bin/runhaskell bin/runghc)
-
-pkg_build_deps=(
-	core/make
-	core/gcc-base
-	core/ncurses
-	core/cabal-install
-)
+pkg_include_dirs=(lib/ghc-"${pkg_version}"/include)
+pkg_interpreters=(bin/runhaskell bin/runghc)
 
 pkg_deps=(
+	core/glibc
+  core/gmp
+  core/ncurses5
 )
 
+pkg_build_deps=(
+	core/gcc
+	core/make
+	core/patchelf
+	core/numactl
+  #core/libffi
+  #core/perl
+)
+
+ghc_patch_rpath() {
+  RELATIVE_TO=$(dirname "$1")
+  RELATIVE_PATHS=$( (for LIB_PATH in "${@:2}"; do echo "\$ORIGIN/$(realpath --relative-to="${RELATIVE_TO}" "${LIB_PATH}")"; done) | paste -sd ':' )
+  patchelf --set-rpath "${LD_RUN_PATH}:${RELATIVE_PATHS}" "$1"
+}
+export -f ghc_patch_rpath
+
+do_prepare() {
+	# moving content to pkg_dirname
+	mv ${HAB_CACHE_SRC_PATH}/ghc-${pkg_version}/* ${CACHE_PATH}
+
+	LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:$(pkg_path_for gmp)/lib: \
+    $(pkg_path_for ncurses5)/lib: \
+    $(pkg_path_for libffi)/lib: \
+    $(pkg_path_for numactl)/lib: \
+    $(pkg_path_for glibc)/lib"
+	export LD_LIBRARY_PATH
+}
+
 do_build() {
-	${CACHE_PATH}/configure --prefix=${pkg_prefix}
+  build_line "Fixing interpreter for binaries:"
+
+  find . -type f -executable \
+    -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
+    -print \
+    -exec patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" {} \;
+
+  
+
+  ./configure --prefix="${pkg_prefix}" \
+    --with-gmp-includes="$(pkg_path_for gmp)/include" \
+    --with-gmp-libraries="$(pkg_path_for gmp)/lib"
 }
 
 do_install() {
-	make install
+  local GHC_LIB_PATHS
+
+  do_default_install
+
+  pushd "${pkg_prefix}"
+
+  #ln -sf $(pkg_path_for ncurses)/lib/libtinfow.so.6.4 ${pkg_prefix}/lib/libtinfo.so.5
+  #patchelf --set-rpath "${pkg_prefix}/lib" ${pkg_prefix}/ghc-8.2.2/bin/ghc-pkg
+
+  GHC_LIB_PATHS=$(find . -name '*.so' -printf '%h\n' | uniq)
+
+  build_line "Fixing rpath for binaries:"
+
+  find . -type f -executable \
+    -exec sh -c 'file -i "$1" | grep -q "x-executable; charset=binary"' _ {} \; \
+    -print \
+    -exec bash -c 'ghc_patch_rpath $1 $2 ' _ "{}" "$GHC_LIB_PATHS" \;
+
+  popd
+}
+
+do_strip() {
+  return 0
 }
--- packages/tools/package-management/cabal-install/plan.sh
+++ packages/tools/package-management/cabal-install/plan.sh
@@ -1,22 +1,22 @@
-pkg_name="cabal-install-stage1"
+pkg_name="cabal-install"
 pkg_origin=core
-pkg_version="3.10.2.0"
+pkg_version="3.10.3.0"
 pkg_license=('')
 pkg_upstream_url="https://www.haskell.org/cabal/"
 pkg_description="Command-line interface for Cabal and Hackage"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source="https://downloads.haskell.org/~cabal/cabal-install-${pkg_version}/cabal-install-${pkg_version}-x86_64-linux-deb9.tar.xz"
-pkg_shasum="af5ce42114cf7720c37fee3238781df4c75bb74914c62e6a68833fb434e0cad7"
-pkg_dirname="${pkg_name}-${pkg_version}"
+pkg_shasum="97e61bcd143f0b8a181ea2261a1de587b58fa9f84d123b9b9cec4463ae2f41c6"
+#pkg_dirname="${pkg_name}-${pkg_version}"
 
 pkg_deps=(
-	core/build-tools-glibc
-	core/build-tools-zlib
-	core/build-tools-gmp
+	core/glibc
+	core/zlib
+	core/gmp
 )
 
 pkg_build_deps=(
-	core/build-tools-patchelf
+	core/patchelf
 )
 
 pkg_bin_dirs=(bin)
@@ -36,8 +36,8 @@ do_install() {
 	mv ${HAB_CACHE_SRC_PATH}/cabal ${pkg_prefix}/bin
 	mv ${HAB_CACHE_SRC_PATH}/plan.json ${pkg_prefix}/bin
 
-	dynamic_linker="$(pkg_path_for build-tools-glibc)/lib/ld-linux-x86-64.so.2"
-	patchelf --set-rpath "$(pkg_path_for build-tools-glibc)/lib:$(pkg_path_for build-tools-zlib)/lib:$(pkg_path_for build-tools-gmp)/lib" --set-interpreter "$dynamic_linker" "$pkg_prefix"/bin/cabal
+	dynamic_linker="$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2"
+	patchelf --set-rpath "$(pkg_path_for glibc)/lib:$(pkg_path_for zlib)/lib:$(pkg_path_for gmp)/lib" --set-interpreter "$dynamic_linker" "$pkg_prefix"/bin/cabal
 }
 
 do_strip() {
