3c759d91fe810997978dfa111f15d438a8a17003
raviduddela <rduddela@progress.com>
2024-07-18 09:33:35 +0000

    build changes for protobuf-rust
    Signed-off-by: raviduddela <rduddela@progress.com>

--- packages/development/libraries/protobuf-rust/plan.sh
+++ packages/development/libraries/protobuf-rust/plan.sh
@@ -8,15 +8,13 @@ pkg_upstream_url="https://github.com/stepancheg/rust-protobuf"
 pkg_description="Rust implementation of Google protocol buffers"
 pkg_deps=(
   core/glibc
-  core/gcc-libs
+  core/gcc-base
 )
 pkg_build_deps=(
   core/rust
-  core/gcc
 )
 
 do_build() {
-  #cargo add protobuf -- --root "${pkg_prefix}" --vers "${pkg_version}" -j"$(nproc)" --verbose
   cargo install protobuf --root "${pkg_prefix}" --vers "${pkg_version}" -j"$(nproc)" --verbose
 }
 
