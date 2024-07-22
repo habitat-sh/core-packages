b8804a1e6e7c04fb65052fdb0b7732a7ed9b74d5
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2024-07-22 11:00:41 +0530

    pkg_source value changed from linux-arm64 to linux-amd64
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- packages/applications/databases/minio/plan.sh
+++ packages/applications/databases/minio/plan.sh
@@ -1,10 +1,10 @@
 pkg_name="minio"
 pkg_origin="core"
-pkg_version="2023-01-02T09-40-09Z"
+pkg_version="2023-11-01T01-57-10Z"
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
-pkg_source="https://dl.minio.io/server/minio/release/linux-arm64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum="0ca0015e8ed4392190c04e21391e9fdcbdbdc5b85622608f120c8e31aa7bdfa5"
+pkg_source="https://dl.minio.io/server/minio/release/linux-amd64/archive/minio.RELEASE.${pkg_version}"
+pkg_shasum="7ab9f2e92e7826d8fc58c983414ec01d8c624965471e8b6c2f720dc3ea71ae17"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
