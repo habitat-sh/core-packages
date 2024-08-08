fe6e6fea8e2ffecf6a2b540c1c6155e2414bf09c
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2024-08-08 11:21:03 +0530

    postgresql-client package added for Windows[CHEF-14850]
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

---
+++ packages/applications/databases/postgresql/15/client/plan.ps1
@@ -0,0 +1,58 @@
+$pkg_name="postgresql15-client"
+$pkg_origin="core"
+$pkg_version="15.6"
+$pkg_license=('PostgreSQL')
+$pkg_upstream_url="https://www.postgresql.org/"
+$pkg_description="PostgreSQL is a powerful, open source object-relational database system."
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_source="https://get.enterprisedb.com/postgresql/postgresql-${pkg_version}-1-windows-x64-binaries.zip"
+$pkg_shasum="64e4cd8ba50aa75c4e6573665f97ce6c215e71d840d88896f82dd89bbc334b6d"
+
+$pkg_deps=@(
+    "core/visual-cpp-redist-2022"
+)
+
+$pkg_bin_dirs=@("bin")
+$pkg_include_dirs=@("include")
+$pkg_lib_dirs=@("lib")
+
+$server_execs=@(
+    "ecpg.exe"
+    "initdb.exe"
+    "pg_archivecleanup.exe"
+    "pg_controldata.exe"
+    "pg_rewind.exe"
+    "pg_test_fsync.exe"
+    "pg_test_timing.exe"
+    "pg_upgrade.exe"
+)
+
+$server_includes=@(
+    "informix"
+    "server"
+)
+
+function Invoke-Install {
+    Push-Location "pgsql"
+
+    foreach ($dir in @("bin","include","lib")) {
+        Copy-Item $dir "$pkg_prefix" -Recurse -Force
+    }
+
+    Write-Host "Purging unneeded execs"
+    foreach ($unneeded in $server_execs) {
+        $target = "$pkg_prefix\bin\$unneeded"
+        Write-Host "Removing $target"
+        Remove-Item -Path $target -Force
+    }
+
+    Write-Host "Purging Unneeded includes"
+    foreach ($unneeded in $server_includes) {
+        $target = "$pkg_prefix\include\$unneeded"
+        Write-Host "Removing $target"
+        Remove-Item -Path $target -Force -Recurse
+    }
+
+    Pop-Location
+}
+
