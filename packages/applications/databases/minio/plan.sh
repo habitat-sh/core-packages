164f2a862a896330d73123dd1ca40424c5af57b8
chef-ci <chef-ci@chef.io>
2024-08-21 13:48:45 +0530

    - Bumps minio from 2023-01-02T09-40-09Z to 2023-01-31T02-24-19Z.

--- packages/applications/databases/minio/plan.sh
+++ packages/applications/databases/minio/plan.sh
@@ -1,10 +1,10 @@
 pkg_name="minio"
 pkg_origin="core"
-pkg_version="2023-01-02T09-40-09Z"
+pkg_version="2023-01-31T02-24-19Z"
 pkg_description="Minio is a high performance distributed object storage server, designed for large-scale private cloud infrastructure."
 pkg_upstream_url="https://minio.io"
 pkg_source="https://dl.minio.io/server/minio/release/linux-arm64/archive/minio.RELEASE.${pkg_version}"
-pkg_shasum="0ca0015e8ed4392190c04e21391e9fdcbdbdc5b85622608f120c8e31aa7bdfa5"
+pkg_shasum="2ede605a84e658993e6868b5a1ab0cee493906901e9cc150153adbf0913fcfcc"
 pkg_license=('Apache-2.0')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_bin_dirs=(bin)
