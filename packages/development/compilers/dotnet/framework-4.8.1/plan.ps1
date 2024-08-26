3174e88a9c5e36ae735cdac0d0846cdcb9d69bcd
John <john.mccrae@progress.com>
2024-08-26 17:18:28 +0000

    Updating the dotnet installer
    Signed-off-by: John <john.mccrae@progress.com>

--- packages/development/compilers/dotnet/framework-4.8.1/plan.ps1
+++ packages/development/compilers/dotnet/framework-4.8.1/plan.ps1
@@ -8,25 +8,21 @@ $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://go.microsoft.com/fwlink/?linkid=2203306"
 $pkg_filename="NDP481-DevPack-ENU.exe"
 $pkg_shasum="fd9ae4b63cbc8f7637249c4b29d25281b1f70fbe6d650109dcbf2bdf596cf994"
-$pkg_build_deps=@("core/lessmsi")
+$pkg_build_deps=@("core/lessmsi", "core/wix")
+
 $pkg_bin_dirs=@("Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools\x64")
 $pkg_lib_dirs=@("Program Files (x86)\Windows Kits\NETFXSDK\4.8.1\Lib\um\x64")
 $pkg_include_dirs=@("Program Files\Windows Kits\NETFXSDK\4.8.1\Include\um")
 
 function Invoke-SetupEnvironment {
-    Set-RuntimeEnv -IsPath "TargetFrameworkRootPath" "$pkg_prefix\Program Files (x86)\Reference Assemblies\Microsoft\Framework"
+    Set-RuntimeEnv -IsPath "TargetFrameworkRootPath" "$pkg_prefix\Program Files\Reference Assemblies\Microsoft\Framework"
 }
 
 function Invoke-Unpack {
-    $x64_bin_path = "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-    if (-not( Test-Path -PathType Container $x64_bin_path)) {
-        Write-Output "My 64-bit path was NOT found, now building here : $x64_bin_path"
-        New-Item -Path $x64_bin_path -ItemType Directory -Force
-      }
-    Start-Process "$HAB_CACHE_SRC_PATH/$pkg_filename" -Wait -ArgumentList "/features OptionId.NetFxSoftwareDevelopmentKit /layout $HAB_CACHE_SRC_PATH/$pkg_dirname /quiet"
+    dark -x "$HAB_CACHE_SRC_PATH/$pkg_dirname" "$HAB_CACHE_SRC_PATH/$pkg_filename"
     Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
     try {
-        Get-ChildItem "*.msi" | ForEach-Object {
+        Get-ChildItem "AttachedContainer/packages" -Include *.msi -Recurse | ForEach-Object {
             lessmsi x $_
         }
     } finally { Pop-Location }
@@ -36,5 +32,4 @@ function Invoke-Install {
     Get-ChildItem "$HAB_CACHE_SRC_PATH/$pkg_dirname" -Include "Program Files" -Recurse | ForEach-Object {
         Copy-Item $_ "$pkg_prefix" -Recurse -Force
     }
-    # Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/Redistributable/4.5.50710/netfx45_dtp/SourceDir/ProgramFilesFolder/*" "$pkg_prefix/Program Files" -Recurse
 }
