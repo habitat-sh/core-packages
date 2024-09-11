795c271aca1192b2f4cb84fca4b485e065d5f705
Nimit <nimit.jyotiana@hotmail.com>
2024-09-11 05:58:29 +0000

    [cairo] updated deps for used in rdeps of package
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- packages/development/libraries/cairo/plan.sh
+++ packages/development/libraries/cairo/plan.sh
@@ -9,50 +9,53 @@ pkg_upstream_url="https://www.cairographics.org"
 pkg_shasum="243a0736b978a33dee29f9cca7521733b78a65b5418206fef7bd1c3d4cf10b64"
 
 pkg_deps=(
-  core/freetype
-  core/glibc
-  core/libice
-  core/libpng
-  core/pixman
-  core/zlib
+	core/freetype
+	core/glibc
+	core/libice
+	core/libpng
+	core/pixman
+	core/zlib
+	core/libxml2
+	core/fontconfig
 )
 pkg_build_deps=(
-  core/fontconfig
-  core/pkg-config
-  core/xextproto
-  core/xproto
-  core/meson
-  core/ninja
-  core/python
-  core/sed
-  core/gcc
+	core/pkg-config
+	core/xextproto
+	core/xproto
+	core/meson
+	core/ninja
+	core/python
+	core/sed
+	core/gcc
+	core/patchelf
 )
 
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(
-  include
-  include/cairo
+	include
+	include/cairo
 )
 pkg_lib_dirs=(
-  lib
-  lib/cairo
+	lib
+	lib/cairo
 )
 pkg_pconfig_dirs=(lib/pkgconfig)
 
 do_build() {
-  export PYTHONPATH=${PYTHONPATH}:$(pkg_path_for meson)/lib/python3.10/site-packages/
+	export PYTHONPATH=${PYTHONPATH}:$(pkg_path_for meson)/lib/python3.10/site-packages/
 
-  sed -e "s,/usr/bin/env python3,$(pkg_path_for python)/bin/python,g" -i "${CACHE_PATH}/version.py"
-  meson setup builddir --prefix=${pkg_prefix} --buildtype=release
-  ninja -C builddir
+	sed -e "s,/usr/bin/env python3,$(pkg_path_for python)/bin/python,g" -i "${CACHE_PATH}/version.py"
+	meson setup builddir --prefix=${pkg_prefix} --buildtype=release
+	# --wrap-mode=nofallback
+	ninja -C builddir
 }
 
 do_install() {
-  ninja -C builddir install
+	ninja -C builddir install
 
-  find "${pkg_prefix}/lib" -type f -name "*.so*" -exec patchelf --set-rpath "${LD_RUN_PATH}" {} \;
+	find "${pkg_prefix}/lib" -type f -name "*.so*" -exec patchelf --set-rpath "${LD_RUN_PATH}" {} \;
 }
 
 do_check() {
-  meson test -C builddir -v cairo
+	meson test -C builddir -v cairo
 }
