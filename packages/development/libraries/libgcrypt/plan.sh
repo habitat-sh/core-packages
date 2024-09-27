e5dac81f9d85a3cafeb259492c4f00ea69e0e3f5
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2024-09-27 15:03:04 +0530

    buildah and deps of buildah added for ARM [CHEF-16009]
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

---
+++ packages/development/libraries/libgcrypt/plan.sh
@@ -0,0 +1,48 @@
+pkg_name=libgcrypt
+pkg_origin=core
+pkg_version=1.10.3
+pkg_license=('LGPL-2.0-or-later')
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="https://gnupg.org/ftp/gcrypt/${pkg_name}/${pkg_name}-${pkg_version}.tar.bz2"
+pkg_shasum=8b0870897ac5ac67ded568dcfadf45969cfa8a6beb0fd60af2a9eadc2a3272aa
+pkg_upstream_url="https://www.gnupg.org/software/libgcrypt/index.html"
+pkg_description="Libgcrypt is a general purpose cryptographic library originally based on code from GnuPG."
+pkg_deps=(
+  core/glibc
+  core/libgpg-error
+)
+pkg_build_deps=(
+  core/gcc
+  core/coreutils
+  core/sed
+  core/bison
+  core/flex
+  core/grep
+  core/bash
+  core/gawk
+  core/libtool
+  core/diffutils
+  core/findutils
+  core/xz
+  core/gettext
+  core/gzip
+  core/make
+  core/patch
+  core/texinfo
+  core/util-linux
+)
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+
+do_build() {
+  ./configure \
+    --prefix="${pkg_prefix}" \
+    --enable-static \
+    --enable-shared
+  make
+}
+
+do_check() {
+  make check
+}
