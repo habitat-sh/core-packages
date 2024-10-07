e341839a68b38d029eebfb3a3a5ffaa7ba7dacec
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2024-10-07 14:34:01 +0530

    Openssl downgraded to 3.0.9 from 3.3.1 for Windows[CHEF-16026
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- packages/development/libraries/openssl/plan.ps1
+++ packages/development/libraries/openssl/plan.ps1
@@ -1,11 +1,11 @@
 $pkg_name="openssl"
 $pkg_origin="core"
-$pkg_version="3.3.1"
+$pkg_version="3.0.9"
 $pkg_description="OpenSSL is an open source project that provides a robust, commercial-grade, and full-featured toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols. It is also a general-purpose cryptography library."
 $pkg_upstream_url="https://www.openssl.org"
 $pkg_license=@("OpenSSL")
 $pkg_source="https://github.com/openssl/openssl/archive/refs/tags/openssl-$pkg_version.zip"
-$pkg_shasum="307284f39bfb7061229c57e263e707655aa80aa9950bf6def28ed63fec91a726"
+$pkg_shasum="e32785cddc5f156c7dafab996b56da84beb05fd1d17efde9792e7d342b7f4588"
 $pkg_deps=@("core/visual-cpp-redist-2022")
 $pkg_build_deps=@("core/visual-build-tools-2022", "core/windows-11-sdk", "core/perl", "core/nasm")
 $pkg_bin_dirs=@("bin")
