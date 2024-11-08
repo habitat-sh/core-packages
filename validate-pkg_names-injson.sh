09e646e4abeef7417c759a834e993953d8cfc01d
Ravi Duddela <RaviShankar.Duddela@progress.com>
2024-11-08 15:17:30 +0530

    add logic for input file and fetching names from windows plans
    Signed-off-by: Ravi Duddela <RaviShankar.Duddela@progress.com>

--- validate-pkg_names-injson.sh
+++ validate-pkg_names-injson.sh
@@ -1,47 +1,83 @@
 #!/bin/bash
-#script to check the package names in the repo has maintainance cycle listed in the
-#category.json file.,thus ensuring all plan names in the repo has maintainance cycle.
-#compares pkg_names extracted from repo with that of package names from json file.
-#Author: Ravi Duddela <rduddela@progress.com>
-#Maintainers: The Habitat Maintainers humans@habitat.sh
-#Usage: jq to be installed for JSON parsing.
-#reconfirm the jsonfile and repodir. here has given default locations.
-#this script to be run from root folder of core-packages repo.
+#
+# script to check the package names in the repo has maintainance cycle listed in the
+# category.json file.,thus ensuring all plan names in the repo has maintainance cycle.
+# compares pkg_names extracted from repo with that of package names from json file.
+#
+# Author: Ravi Duddela <rduddela@progress.com>
+# Maintainers: The Habitat Maintainers humans@habitat.sh
+#
+# Usage: jq to be installed for JSON parsing.
+# takes category json file as input
+# sh validate-pkg_names-injson.sh </path/to/category.json file>
+#
 
-get_names_from_repo() {
+# exit if the category json file is not provided
+if [ -z "$1" ]; then
+  echo "---Error: Please specify category.json file---"
+  exit 1
+fi
+
+# check if provided file exists
+if [ ! -f "$1" ]; then
+  echo "---Error: File $1 doesnt exist.Please verify---"
+  exit 1
+fi
+
+# define functions
+# get pkg names from plan.sh files
+get_linux_pkgnames_from_repo() {
   (
-    local repodir="${1}"
-    cd "$repodir"
-    for plan in $( find . -name plan.sh ); do
+    for plan in $( find $repo_root_dir -name plan.sh ); do
     source "$plan"
     echo "${pkg_name}"
     done
   )
 }
 
-get_names_from_json() {
+# get pkg names from json file
+get_pkgnames_from_json() {
   (
     local jsonfile="$1"
-    jq -r .data[].pkg_name category.json
+    jq -r .data[].pkg_name $jsonfile
   )
 }
 
-jsonfile=category.json  #here can give path of json file if not present in pwd.
-repodir="$PWD"  #can change the path of repo if not run from pwd.
+# get pkg names from plan.ps1 files
+get_win_pkgnames_from_repo() {
+  (
+    find $repo_root_dir -type f -name "*.ps1" | while IFS= read -r file; do
+	    pkg_name=$(sed -n 's/.*pkg_name="\([^"]*\)".*/\1/p' "$file")
+	    if [ -n "$pkg_name" ]; then
+    	echo "${pkg_name}"
+      fi
+    done
+  )
+}
 
-repo_pkg_names=$(get_names_from_repo "$repodir")
-json_pkg_names=$(get_names_from_json "$jsonfile")
+repo_root_dir=$(git rev-parse --show-toplevel)
+jsonfile="$1"
 
-# Convert names to arrays
+repo_pkg_names=$(get_linux_pkgnames_from_repo "$repodir")
+repo_win_pkg_names=$(get_win_pkgnames_from_repo "$repodir")
+json_pkg_names=$(get_pkgnames_from_json "$jsonfile")
+
+
+# convert names to arrays
 IFS=$'\n' read -rd '' -a json_array <<< "$json_pkg_names"
-IFS=$'\n' read -rd '' -a repo_array <<< "$repo_pkg_names"
+IFS=$'\n' read -rd '' -a linux_array <<< "$repo_pkg_names"
+IFS=$'\n' read -rd '' -a win_array <<< "$repo_win_pkg_names"
 
-#compare each element with that of json array
+# concatenate linux package array and window packages array
+repo_array=("${linux_array[@]}" "${win_array[@]}")
+
+
+# compare each element with that of json array
 for pkg_name in "${repo_array[@]}"; do
   found=false
   for json_name in "${json_array[@]}"; do
     if [[ "$pkg_name" == "$json_name" ]] ; then
-      found=true #name found
+      found=true # name found
       break
     fi
   done
